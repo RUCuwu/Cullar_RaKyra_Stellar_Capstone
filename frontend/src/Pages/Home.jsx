5e257c1d7f9ee109344ee7bd923332c825077d15
RUCuwu <afiji.x04@gmail.com>
2024-10-16 14:13:39 -0500

    add service link

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import About from "../components/About";
+import Qualtiies from "../components/Qualities";
 
 const Home = () => {
     return (
         <>
             <HeroSection />
             <About />
+            
+            
+            
+            <Qualtiies />
+            
         </>
     )
 };
