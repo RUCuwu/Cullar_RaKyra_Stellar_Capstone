3f592bb443191103ccf316270d0a1546f6dc9f70
RUCuwu <afiji.x04@gmail.com>
2024-10-16 16:29:51 -0500

    added reservation slots

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import Qualitiies from "../components/Qualities";
 import Menu from "../components/Menu";
 import WhoAreWe from "../components/WhoAreWe";
 import Team from "../components/Team";
+import Reservation from "../components/Reservation";
 
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
             <Menu />
             <WhoAreWe />
             <Team />
+            <Reservation />
             
         </>
     )
